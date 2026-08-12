2ef73a8c4d71d5f89d6211dc187f57d106853ab4
govardhan <govardhanreddy.g2005@gmail.com>
2026-08-12 13:01:08 +0530

    implement for loop to manipulate array logic

--- solutions/transpose_flatten/transpose_and_flatten.py
+++ solutions/transpose_flatten/transpose_and_flatten.py
@@ -1,3 +1,16 @@
 import numpy as np
 
 n,m = map(int, input().split())
+
+arr = []
+
+for _ in range(n):
+    arr.append(input().split())
+    
+array = np.array(arr, dtype=int).reshape(n,m)
+
+inverse = array.T
+
+print(inverse)
+
+print(array.flatten())
