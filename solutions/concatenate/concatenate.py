406a33c42134a0b1fcf38a5b3f004ba614b14f96
govardhan <govardhanreddy.g2005@gmail.com>
2026-08-13 11:11:54 +0530

    complete concatenate task

--- solutions/concatenate/concatenate.py
+++ solutions/concatenate/concatenate.py
@@ -1,1 +1,19 @@
 import numpy as np
+
+N,M,P = map(int, input().split())
+
+columns_A = []
+
+for _ in range(N):
+    columns_A.append(input().split())
+    
+columns_B = []
+
+for _  in range(M):
+    columns_B.append(input().split())
+    
+A = np.array(columns_A, dtype=int)
+B = np.array(columns_B, dtype=int)
+    
+combined = np.concatenate([A,B], axis=0)
+print(combined)
