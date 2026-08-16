55fb4561d3876f199ed68020f84668e5da28b01a
govardhan <govardhanreddy.g2005@gmail.com>
2026-08-16 12:23:58 +0530

    implement actual logic of array mathematics

--- solutions/array_mathematics/mathematics_oper.py
+++ solutions/array_mathematics/mathematics_oper.py
@@ -11,3 +11,14 @@ columns_B = []
 
 for _ in range(n):
     element = columns_B.extend(input().split())
+
+array_A = np.array(columns_A, dtype=int).reshape(n,m)
+
+array_B = np.array(columns_B, dtype=int).reshape(n,m)
+
+print(array_A + array_B)
+print(array_A - array_B)
+print(array_A * array_B)
+print(np.floor_divide(array_A, array_B))
+print(array_A % array_B)
+print(array_A ** array_B)
