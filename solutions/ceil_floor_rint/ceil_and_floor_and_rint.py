ed62b20d51c5676ed919a3167a3cd2dc2c13034e
govardhan <govardhanreddy.g2005@gmail.com>
2026-08-19 10:20:56 +0530

    implement problem logic

--- solutions/ceil_floor_rint/ceil_and_floor_and_rint.py
+++ solutions/ceil_floor_rint/ceil_and_floor_and_rint.py
@@ -1,1 +1,9 @@
 import numpy as np
+
+np.set_printoptions(legacy="1.13")
+
+scalar = np.array(list(map(float, input().split())))
+
+print(np.floor(scalar))
+print(np.ceil(scalar))
+print(np.rint(scalar))
